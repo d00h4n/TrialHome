9cdf8e80ab06cedf725c5296680249a6417e5517
d00h4n <doohan.mintarso@gmail.com>
2023-06-09 11:04:13 +0700

    Day4

--- TrialScrambel/Program.cs
+++ TrialScrambel/Program.cs
@@ -1,8 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 
-
-
+enum LetterPoints
+{
+    A = 1,
+    B = 3,
+    C = 3,
+    D = 2,
+    E = 1,
+    F = 4,
+    G = 2,
+    H = 4,
+    I = 1,
+    J = 8,
+    K = 5,
+    L = 1,
+    M = 3,
+    N = 1,
+    O = 1,
+    P = 3,
+    Q = 10,
+    R = 1,
+    S = 1,
+    T = 1,
+    U = 1,
+    V = 4,
+    W = 4,
+    X = 8,
+    Y = 4,
+    Z = 10
+}
 
 class Program
 {
@@ -11,17 +37,21 @@ class Program
         int rows = 15;
         int columns = 15;
 
-        DisplayBoard(rows, columns);
-    }
-
-    static void DisplayBoard(int rows, int columns)
-    {
         char[,] board = new char[rows, columns];
         int score = 0;
 
         Console.WriteLine("Scramble Board:");
+        Console.WriteLine();
+        Console.Write("   ");
+        for (int column = 0; column < columns; column++)
+        {
+            Console.Write($" {column.ToString().PadLeft(2)} ");
+        }
+        Console.WriteLine();
+
         for (int row = 0; row < rows; row++)
         {
+            Console.Write($"{row.ToString().PadLeft(2)} ");
             for (int column = 0; column < columns; column++)
             {
                 Console.Write("[ ] ");
@@ -29,6 +59,8 @@ class Program
             }
             Console.WriteLine();
         }
+
+        Console.WriteLine();
 
         Console.WriteLine("Enter coordinates and letters to fill in the scramble board:");
         while (true)
@@ -64,15 +96,28 @@ class Program
                     continue;
                 }
 
+                Console.WriteLine();
+
                 Console.WriteLine("Updated Scramble Board:");
+                Console.WriteLine();
+                Console.Write("   ");
+                for (int column = 0; column < columns; column++)
+                {
+                    Console.Write($" {column.ToString().PadLeft(2)} ");
+                }
+                Console.WriteLine();
+
                 for (int row = 0; row < rows; row++)
                 {
+                    Console.Write($"{row.ToString().PadLeft(2)} ");
                     for (int column = 0; column < columns; column++)
                     {
                         Console.Write($"[{board[row, column]}] ");
                     }
                     Console.WriteLine();
                 }
+
+                Console.WriteLine();
 
                 int points = GetLetterPoints(letter);
                 Console.WriteLine($"Letter '{letter}' is worth {points} points.");
@@ -83,9 +128,20 @@ class Program
             }
         }
 
+        Console.WriteLine();
+
         Console.WriteLine("Final Scramble Board:");
+        Console.WriteLine();
+        Console.Write("   ");
+        for (int column = 0; column < columns; column++)
+        {
+            Console.Write($" {column.ToString().PadLeft(2)} ");
+        }
+        Console.WriteLine();
+
         for (int row = 0; row < rows; row++)
         {
+            Console.Write($"{row.ToString().PadLeft(2)} ");
             for (int column = 0; column < columns; column++)
             {
                 Console.Write($"[{board[row, column]}] ");
@@ -93,12 +149,21 @@ class Program
             Console.WriteLine();
         }
 
+        Console.WriteLine();
+
         Console.WriteLine($"Final Score: {score} points");
     }
 
     static int GetLetterPoints(char letter)
     {
-        LetterPoints letterPoints = (LetterPoints)Enum.Parse(typeof(LetterPoints), letter.ToString());
-        return (int)letterPoints;
+        if (Enum.TryParse(letter.ToString().ToUpper(), out LetterPoints letterPoints))
+        {
+            return (int)letterPoints;
+        }
+        else
+        {
+            Console.WriteLine("Invalid letter! Assigning 0 points.");
+            return 0;
+        }
     }
 }
